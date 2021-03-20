e0db341c92e18f458cadbbbcf0a997e093e89ca5
ericcapiz <ericcapiz@gmail.com>
2021-03-20 14:06:31 -0500

    MVP

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,114 @@
 // Write your "actions" router here!
+const {Router} = require('express');
+const express = require('express');
+const Actions = require('./actions-model');
+const router = express.Router();
+
+//GET ACTIONS
+
+router.get('/', (req, res) => {
+    Actions
+        .get()
+        .then(actions => {
+            res
+                .status(200)
+                .json(actions);
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({message: 'error retrieving actions'});
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    Actions
+        .get(id)
+        .then(actions => {
+            if (actions) {
+                res
+                    .status(200)
+                    .json(actions);
+            } else {
+                res
+                    .status(404)
+                    .json({message: 'Action not found with that id'})
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({message: 'error getting actions by id'})
+        })
+})
+
+//POST ACTIONS
+
+router.post("/", (req, res) => {
+    const newAction = req.body;
+
+    if (!req.body.project_id || !req.body.description || !req.body.notes) {
+        res
+            .status(400)
+            .json({message: "missing info to create new action"});
+    } else {
+        Actions
+            .insert(newAction)
+            .then((newAct) => {
+                res
+                    .status(201)
+                    .json(newAct);
+            })
+            .catch((error) => {
+                res
+                    .status(500)
+                    .json({message: error.message});
+            });
+    }
+});
+
+//PUT ACTIONS
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+  
+    if (!req.body.project_id || !req.body.description || !req.body.notes) {
+      res.status(400).json({ errorMessage: "missing info to update" });
+    } else {
+      Actions.update(id, update)
+        .then((updated) => {
+          res.status(200).json(updated);
+        })
+        .catch((error) => {
+          res.status(500).json({ error: error.message });
+        });
+    }
+  });
+
+//DELETE ACTIONS
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+  
+    Actions.remove(id)
+      .then((deleteAct) => {
+        if (deleteAct > 0) {
+          res
+            .status(200)
+            .json({ message: `successfully deleted action id ${id}` });
+        } else {
+          res
+            .status(404)
+            .json({ message: `action with id ${id} does not exist.` });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  });
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,106 @@
 // Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+const router = express.Router();
+
+//GET PROJECTS
+
+router.get('/', (req, res) => {
+    Projects
+        .get()
+        .then(projects => {
+            res
+                .status(200)
+                .json(projects);
+        })
+        .catch(error =>{
+            console.log(error);
+            res.status(500).json({message: 'error retrieving projects'});
+        })
+})
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    Projects.get(id)
+    .then(project => {
+        if(!project){
+            res.status(404).json({ message: `post with id ${id} does not exist.` })
+        } else {
+            res.status(200).json(project)
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: error.message })
+    })
+})
+
+//POST PROJECTS
+
+router.post("/", (req, res) => {
+
+    if(!req.body.name || !req.body.description){
+        res.status(400).json({ message: "missing required info, either name or description."})
+    } else {
+        Projects.insert(req.body)
+        .then(newProj => {
+            res.status(201).json(newProj)
+        })
+        .catch(error => {
+            res.status(500).json({ message: error.message })
+        })
+    }
+})
+
+//PUT PROJECTS
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+
+    if(!req.body.name || !req.body.description){
+        res.status(400).json({ message: "req.body missing either name or description to be updated."})
+    } else{
+        Projects.update(id, req.body)
+        .then(updated => {
+            res.status(200).json(updated)
+        })
+        .catch(error => {
+            res.status(500).json({ message: error.message })
+        })
+    }
+
+})
+
+//DELETE PROJECTS
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    Projects.remove(id)
+    .then(delProj => {
+        if(delProj > 0) {
+            res.status(200).json({ message: `successfully deleted post id ${id}`})
+        } else {
+            res.status(404).json({ message: `post with id ${id} does not exist.` })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: error.message })
+    })
+})
+
+//GET PROJECTS ACTIONS
+
+router.get("/:id/actions", (req, res) => {
+    const { id } = req.params;
+
+    Projects.getProjectActions(id)
+    .then(projAct => {
+        res.status(200).json(projAct)
+    })
+    .catch(error => {
+        res.status(500).json({ message: error.message })
+    })
+})
+
+module.exports = router;
